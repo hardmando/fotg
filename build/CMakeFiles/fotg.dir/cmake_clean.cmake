2704c67f95d550a3187ac44f77ce0677551a010c
hardmando <hardmando1932@gmail.com>
2025-10-26 06:09:10 -0500

    initialization separated from main.cpp

---
+++ build/CMakeFiles/fotg.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fotg.dir/link.d"
+  "CMakeFiles/fotg.dir/src/init.cpp.o"
+  "CMakeFiles/fotg.dir/src/init.cpp.o.d"
+  "CMakeFiles/fotg.dir/src/main.cpp.o"
+  "CMakeFiles/fotg.dir/src/main.cpp.o.d"
+  "fotg"
+  "fotg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fotg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
